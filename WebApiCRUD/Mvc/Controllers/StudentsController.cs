970b10f904d8cc80c9f2d9b1efdcb0694019b61f
subrato <subrato2393@gmail.com>
2021-02-12 00:00:06 +0600

    Call Web Api using Asp.net MVC view

---
+++ WebApiCRUD/Mvc/Controllers/StudentsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Mvc.Controllers
+{
+    public class StudentsController : Controller 
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<StudentModel> reservationList = new List<StudentModel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:44325/api/students"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    reservationList = JsonConvert.DeserializeObject<List<StudentModel>>(apiResponse);
+                }
+            }
+            return View(reservationList);
+        }
+    }
+}
---
+++ WebApiCRUD/Mvc/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNo { get; set; }
+    }
+}
