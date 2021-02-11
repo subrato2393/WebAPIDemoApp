018b3387260a4f8e78f0f72354bc8a0e279e47b7
subrato <subrato2393@gmail.com>
2021-02-11 13:01:53 +0600

    add connectionstring in default process

---
+++ WebApiCRUD/WebApiCRUD/Contexts/ApiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCRUD.Models;
+
+namespace WebApiCRUD.Contexts
+{
+    public class ApiDbContext:DbContext
+    {
+        private string _connectionString;
+        public DbSet<Student> Students { get; set; }
+        public ApiDbContext()
+        {
+            _connectionString = @"Server=DESKTOP-G2KIJII\SQLEXPRESS;Database=apidb6;User Id=sa;Password=subroto";
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
